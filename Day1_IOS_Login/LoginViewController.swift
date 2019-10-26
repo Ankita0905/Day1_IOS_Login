035a755f73a5dcde33dd3a7747e85ca7552c839c
ankita <ankita.jain12342@gmail.com>
2019-10-26 19:19:39 -0400

    added buttons

--- Day1_IOS_Login/LoginViewController.swift
+++ Day1_IOS_Login/LoginViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var txtUserName: UITextField!
+    @IBOutlet weak var txtUserPassword: UITextField!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnLoginClick(_ sender: UIButton)
+    {
+        let uname=txtUserName.text!
+        print("Hello\(uname)")
+    }
+    
 }
 
