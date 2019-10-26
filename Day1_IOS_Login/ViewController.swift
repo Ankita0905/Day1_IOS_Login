7d58d0fafaf649e52aa95b1d8f6aab723adb63d1
ankita <ankita.jain12342@gmail.com>
2019-10-26 19:01:29 -0400

    Created login Screen in IOS

---
+++ Day1_IOS_Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day1_IOS_Login
+//
+//  Created by MacStudent on 2019-10-26.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
