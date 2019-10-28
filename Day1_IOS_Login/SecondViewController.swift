2f2c26ed9b40689825e259626f4267b40dcff9b8
Ankita Jain <ankita.jain12342@gmail.com>
2019-10-28 18:34:14 -0400

    created second screen and other properties

---
+++ Day1_IOS_Login/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  Day1_IOS_Login
+//
+//  Created by Ankita Jain on 2019-10-28.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+}
